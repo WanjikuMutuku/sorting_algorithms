f662cddaf0391d1d33a9259bf407823c79621382
BildadM <bildadmuchemi.w@gmail.com>
2024-01-23 01:19:15 -0800

    insertion and quick sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,23 +2,23 @@
 
 /**
  * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
- * @h: A pointer to the head of the doubly-linked list.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
+ * @head: A pointer to the head of the doubly-linked list.
+ * @point1: A pointer to the first node to swap.
+ * @point2: The second node to swap.
  */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+void swap_nodes(listint_t **head, listint_t **point1, listint_t *point2)
 {
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
+	(*point1)->next = point2->next;
+	if (point2->next != NULL)
+		point2->next->prev = *point1;
+	point2->prev = (*point1)->prev;
+	point2->next = *point1;
+	if ((*point1)->prev != NULL)
+		(*point1)->prev->next = point2;
 	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
+		*head = point2;
+	(*point1)->prev = point2;
+	*point1 = point2->prev;
 }
 
 /**
@@ -30,14 +30,14 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
+	listint_t *iter, *insert, *temp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	for (iter = (*list)->next; iter != NULL; iter = temp)
 	{
-		tmp = iter->next;
+		temp = iter->next;
 		insert = iter->prev;
 		while (insert != NULL && iter->n < insert->n)
 		{
